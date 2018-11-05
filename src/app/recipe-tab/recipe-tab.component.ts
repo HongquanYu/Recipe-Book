a3d790d99c84f4f71177273bbf38310ddee80eec
Hongquan Yu <hyu12346@terpmail.umd.edu>
2018-11-04 21:39:14 -0500

    Reorganize the components, showing data needs to be done

--- src/app/recipe-tab/recipe-tab.component.ts
+++ src/app/recipe-tab/recipe-tab.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-recipe-tab',
   templateUrl: './recipe-tab.component.html',
   styleUrls: ['./recipe-tab.component.css']
 })
 export class RecipeTabComponent implements OnInit {
+  private hasData: boolean;
+  public Recipes: Recipe[] = [];
+  private btnClicked: boolean;
+  private resultAmount: number = 0;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.hasData = this.dataService.hasData;
+    this.Recipes = this.dataService.Recipes;
   }
 
 }
--- src/app/recipe-tab/recipelist/recipelist.component.ts
+++ src/app/recipe-tab/recipelist/recipelist.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from '../data.service';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipelist',
   templateUrl: './recipelist.component.html',
-  styleUrls: ['./recipelist.component.css']
+  styleUrls: ['./recipelist.component.css'],
+  providers: [DataService]
 })
 export class RecipelistComponent implements OnInit {
+  @Input() Recipes: Recipe[];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
+  onGetData(form: NgForm) {
+    this.dataService.onGetData(form);
+  }
 }
